0f77b634ce2ba8a366b2ea4d9529e779353d4f6a
Elias Norberg <xyzzy@kudzu.se>
2014-10-24 11:34:09 +0200

    improve import

--- books/management/commands/import.py
+++ books/management/commands/import.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from books.models import Book
 from django.core.files import File
 import os
+import shutil
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -13,7 +14,9 @@ class Command(BaseCommand):
                     b = Book()
                     b.image = File(f)
                     thumbnail = os.path.join(os.path.dirname(filename),
-                    'thumbnails', os.path.basename(filename))
+                    'thumbnails',
+                    os.path.splitext(os.path.basename(filename))[0] + '.png')
                     with open(thumbnail, "rb") as tn:
                         b.thumbnail = File(tn)
                         b.save()
+                shutil.move(filename, "files/done")
