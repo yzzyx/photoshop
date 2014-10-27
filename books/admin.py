bee0824b389ed73fd2c83bbf0d8628dd5afaf694
Elias Norberg <xyzzy@kudzu.se>
2014-10-27 08:46:11 +0100

    Show who claimed a book in admin interface

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from models import Book, Person
 
+class PersonInline(admin.TabularInline):
+    model = Person
+
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('id', 'image', 'available')
+    list_display = ('id', 'image', 'available', 'claimed_by_list')
     ordering = ('id',)
+    inlines = [ PersonInline, ]
+
+    def claimed_by_list(self, obj):
+        return ",".join([k.name for k in obj.claimed_by.all()])
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
